ea02e8d10f5d0776111bb55ec0121966a275f653
ishant1809 <ishantbhandari1809@gmail.com>
2026-06-12 00:44:25 +0530

    Implement Companion Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,43 @@
-import { Button } from "@/components/ui/button"
+import CTA from "@/components/CTA"
+import CompanionCard from "@/components/ui/CompanionCard"
+import CompanionsList from "@/components/ui/CompanionsList"
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline"> WELCOME TO MY SAAS APP</h1>
-      <Button>
-        Start
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="2"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="science"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="3"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
