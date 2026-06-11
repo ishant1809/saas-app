f3e9678c8a49e51cf341857ce20e156a7f86936f
ishant1809 <ishantbhandari1809@gmail.com>
2026-06-11 22:41:40 +0530

    Initial Commit

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <Button>Sign In</Button>
+        </div>
+    )
+}
+
+export default page
